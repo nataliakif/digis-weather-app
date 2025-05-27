c26053efc723174d41366f78332effb36ef37014
nataliakif <nataliakif@gmail.com>
2025-05-27 17:19:48 +0200

    Add SearchBar Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import CitySearch from "./components/CitySearch";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1 style={{ textAlign: "center" }}>Weather Forecast</h1>
+      <CitySearch />
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/components/CitySearch.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCity,
+  fetchWeather,
+  clearWeather,
+} from "../app/features/weather/weatherSlice";
+import styles from "./CitySearch.module.css";
+
+const CitySearch = () => {
+  const [inputValue, setInputValue] = useState("");
+  const dispatch = useDispatch();
+  const { error, status } = useSelector((state) => state.weather);
+
+  const handleSearch = () => {
+    const city = inputValue.trim();
+    if (!city) return;
+    dispatch(clearWeather());
+    dispatch(setCity(city));
+    dispatch(fetchWeather(city));
+    // console.log(city);
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <input
+        type="text"
+        value={inputValue}
+        placeholder="Enter city name"
+        onChange={(e) => setInputValue(e.target.value)}
+        className={styles.input}
+      />
+      <button onClick={handleSearch} className={styles.button}>
+        Search
+      </button>
+
+      {status === "failed" && error && <p className={styles.error}>{error}</p>}
+    </div>
+  );
+};
+
+export default CitySearch;
